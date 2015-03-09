2fc97737db6d5bea03319f6f4f25036b2f1fd621
PoisonedMinds <steven_biro@hotmail.com>
2015-03-09 14:19:13 -0400

    done

--- ComboLockDriver/src/combolockdriver/ComboLockDriver.java
+++ ComboLockDriver/src/combolockdriver/ComboLockDriver.java
@@ -12,19 +12,36 @@ import javax.swing.*;
 public class ComboLockDriver {
 
     public static void main(String[] args) {
-        int test, num1, num2, num3;
-        String buttons[] = {"Combo Known", "Combo Unknown"};
+        int test, num1, num2, num3, i = 0;
+        String buttons[] = {"Combo Known", "Combo Unknown"}, combo;
+        ComboLock lock = null;
         test = JOptionPane.showOptionDialog(null, "Do you know to combo , or are you guessing it?", "ComboLock",
                 JOptionPane.PLAIN_MESSAGE, 3, null, buttons, buttons[0]);
         if (test == 0) {
-            num1 = Integer.parseInt(JOptionPane.showInputDialog("Input the first number for the lock. \n(0-60"));
-            num2 = Integer.parseInt(JOptionPane.showInputDialog("Input the second number for the lock. \n(0-60"));
-            num3 = Integer.parseInt(JOptionPane.showInputDialog("Input the third number for the lock. \n(0-60"));
-            ComboLock lock =new ComboLock(num1,num2,num3);
+            num1 = Integer.parseInt(JOptionPane.showInputDialog("Input the first number for the lock. \n(0-60)"));
+            num2 = Integer.parseInt(JOptionPane.showInputDialog("Input the second number for the lock. \n(0-60)"));
+            num3 = Integer.parseInt(JOptionPane.showInputDialog("Input the third number for the lock. \n(0-60)"));
+            lock = new ComboLock(num1, num2, num3);
         } else if (test == 1) {
-            ComboLock lock =new ComboLock();
+            lock = new ComboLock();
         }
+        do {
+            i += 1;
+            num1 = Integer.parseInt(JOptionPane.showInputDialog("Guess the first number."));
+            num2 = Integer.parseInt(JOptionPane.showInputDialog("Guess the second number."));
+            num3 = Integer.parseInt(JOptionPane.showInputDialog("Guess the third number."));
+            combo = num1 + ", " + num2 + ", " + num3;
+            if (combo.equals(lock.getCombo())) {
+                break;
+            }
 
+            JOptionPane.showMessageDialog(null, "Incorrect, try again.");
+        } while (i < 3);
+        if (i == 3) {
+            JOptionPane.showMessageDialog(null, "The correct combonation was " + lock.getCombo());
+        } else {
+            JOptionPane.showMessageDialog(null, "You guessed the correc combonation.");
+        }
     }
 
 }
